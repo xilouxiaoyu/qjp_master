d7bd6585e06024294fb1c97f48ecd3955cd51a58
apple <haoxiaoqing@iqianjin.com>
2019-09-24 15:28:09 +0800

    readfile

--- src/main/java/cn/itcast/gjp/service/SortService.java
+++ src/main/java/cn/itcast/gjp/service/SortService.java
@@ -1,5 +1,7 @@
 package cn.itcast.gjp.service;
 
+import java.io.*;
+
 /**
  * @author apple
  * @date 2019/9/24 上午11:27
@@ -17,11 +19,18 @@ public class SortService {
         }
 
     }
-    public static void main(String[] args) {
+
+    public static void readFile(File file) throws IOException{
+        FileInputStream fileInputStream = new FileInputStream(file);
+        System.out.println(fileInputStream.read());
+        fileInputStream.close();
+    }
+    public static void main(String[] args) throws IOException {
         int a[]={1,10,3};
         sort(a);
         for(Object obj:a){
         System.out.println(obj);
+        readFile(new File("/User/a/123.txt"));
         }
     }
 }
