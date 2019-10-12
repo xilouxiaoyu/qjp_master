92aa06ba5c19cd95d743d6c0c90d47e29253b82e
apple <haoxiaoqing@iqianjin.com>
2019-10-12 10:53:17 +0800

    exe3

--- src/main/java/cn/itcast/gjp/service/Exercise1.java
+++ src/main/java/cn/itcast/gjp/service/Exercise1.java
@@ -11,6 +11,9 @@ import java.util.Stack;
  */
 public class Exercise1 {
 
+    /**
+     * 计算字符串中含有另一个字符串的个数
+     */
     public static int  getCount(String str,String key){
         if(str == null || key == null || "".equals(str.trim()) || "".equals(key.trim())){
             return 0;
@@ -23,7 +26,9 @@ public class Exercise1 {
         }
         return count;
     }
-
+    /**
+     * 将两个链表合并
+     */
        /*public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
         if (l1 == null) {
@@ -44,6 +49,10 @@ public class Exercise1 {
 
     }*/
 
+
+    /**
+     * 判断字符串中括号是否匹配出现的
+     */
     public static boolean isComplete(String s){
         Stack<String> left=new Stack<String>();
         while (!s.isEmpty()){
@@ -77,6 +86,71 @@ public class Exercise1 {
         return left.isEmpty();
     }
 
+
+    /**
+     * 用字符串模拟两个大数相加
+     * @param n1 加数1
+     * @param n2 加数2
+     * @return   相加结果
+     */
+
+    public static String add2(String n1,String n2) {
+        StringBuffer result = new StringBuffer();
+
+        //1、反转字符串
+        n1 = new StringBuffer(n1).reverse().toString();
+        n2 = new StringBuffer(n2).reverse().toString();
+
+        int len1 = n1.length();
+        int len2 = n1.length();
+        int maxLen = len1 > len2 ? len1 : len2;
+        boolean nOverFlow = false; //是否越界
+        int nTakeOver = 0 ; //溢出数量
+
+        //2.把两个字符串补齐，即短字符串的高位用0补齐
+        if(len1 < len2)
+        {
+            for(int i = len1 ; i < len2 ; i++)
+            {
+                n1 += "0";
+            }
+        }
+        else if (len1 > len2)
+        {
+            for(int i = len2 ; i < len1 ; i++)
+            {
+                n2 += "0";
+            }
+        }
+
+        //3.把两个正整数相加，一位一位的加并加上进位
+        for(int i = 0 ; i < maxLen ; i++)
+        {
+            int nSum = Integer.parseInt(n1.charAt(i) +"") + Integer.parseInt(n2.charAt(i) +"");
+
+            if(nSum >= 10)
+            {
+                if(i == (maxLen - 1))
+                {
+                    nOverFlow  = true;
+                }
+                nTakeOver = 1;
+                result.append(nSum - 10);
+            }
+            else
+            {
+                nTakeOver = 0;
+                result.append(nSum);
+            }
+        }
+
+        //如果溢出的话表示位增加了
+        if(nOverFlow)
+        {
+            result.append(nTakeOver);
+        }
+        return result.reverse().toString();
+    }
 
     public static void main(String[] args) {
         //System.out.println(getCount("akakkahaha","ha"));
