448d3c8ba370c18e304ea329250e903c91dc5022
apple <haoxiaoqing@iqianjin.com>
2019-12-05 18:55:27 +0800

    member

--- src/main/java/cn/itcast/gjp/service/resTemplateStudy.java
+++ src/main/java/cn/itcast/gjp/service/resTemplateStudy.java
@@ -1,11 +1,8 @@
 package cn.itcast.gjp.service;
 
-import cn.itcast.gjp.domain.user;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -18,11 +15,11 @@ import org.testng.annotations.Test;
 public class resTemplateStudy{
 
     @Test
-    public  void queryBaidu() throws RestClientException, JSONException {
+    public  void queryMember() throws RestClientException, JSONException {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity(headers);
-        ResponseEntity<String> exchange = restTemplate.exchange("http://ckmobile-nginx-10-10-76-162.okd.iqianjin.test/userMemberLevel/sendRelegationAward", HttpMethod.GET, (HttpEntity<?>) requestEntity,String.class);
+        ResponseEntity<JSONObject> exchange = restTemplate.exchange("http://member-center-server-nginx-10-10-76-162.okd.iqianjin.test/userMemberCenter/userMemberLevel/sendRelegationAward", HttpMethod.GET, (HttpEntity<?>) requestEntity,JSONObject.class);
     }
 }
